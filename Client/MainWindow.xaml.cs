07e0d674533113bbc853f27887686e1e44cd17ed
Hwi Jae, Shin <pkallru@gmail.com>
2017-10-01 18:41:42 +0900

    토렌트 클라이언트 R&D 중

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,11 +1,20 @@
 ﻿using AniFile3.DataStruct;
 using CoreLib.MessagePackets;
 using MahApps.Metro.Controls;
+using MonoTorrent;
+using MonoTorrent.Client;
+using MonoTorrent.Client.Encryption;
+using MonoTorrent.Common;
+using MonoTorrent.Dht;
+using MonoTorrent.Dht.Listeners;
 using RichGrassHopper.Core.IO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,7 +43,7 @@ namespace AniFile3
 
             _tempResponse = new List<string>();
 
-            Console.SetOut(new LogWriter(_testLog));
+            //Console.SetOut(new LogWriter(_testLog));
 
             _subscriptions = new Subscriptions();
 
@@ -67,9 +76,80 @@ namespace AniFile3
             var bbb = MessagePack.Deserialize<List<string>>(aaa);
         }
         
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
             //_SerachText.Text
+
+            //string magnetLink = @"magnet:?xt=urn:btih:B4D3A91D9CE527AA7C5B6EDACB969E5486B76EF2&dn= tvN  삼시세끼 바다목장편.E09.170929.720p-NEXT&tr=http://720pier.ru/tracker/announce.php?passkey=3t3mrdei39bxn7x9114tv9vy1reubs8g&tr=http://mgtracker.org:2710/announce&tr=http://tracker.kamigami.org:2710/announce&tr=http://tracker.mg64.net:6881/announce&tr=http://tracker2.wasabii.com.tw:6969/announce&tr=udp://9.rarbg.me:2720/announce&tr=udp://bt.xxx-tracker.com:2710/announce&tr=udp://opentrackr.org:1337/announce&tr=udp://www.eddie4.nl:6969/announce&tr=udp://zer0day.to:1337/announce&tr=udp://tracker1.wasabii.com.tw:6969/announce&tr=udp://185.50.198.188:1337/announce&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker2.indowebster.com:6969/announce&tr=udp://tracker.coppersurfer.tk:6969&tr=http://bt.ttk.artvid.ru:6969/announce&tr=http://bt.artvid.ru:6969/announce&tr=udp://thetracker.org./announce&tr=udp://tracker4.piratux.com:6969/announce&tr=udp://tracker.zer0day.to:1337/announce&tr=udp://62.212.85.66:2710/announce&tr=udp://eddie4.nl:6969/announce&tr=udp://public.popcorn-tracker.org:6969/announce&tr=udp://tracker.grepler.com:6969/announce&tr=http://tracker.dler.org:6969/announce&tr=http://tracker.tiny-vps.com:6969/announce&tr=http://tracker.filetracker.pl:8089/announce&tr=http://tracker.tvunderground.org.ru:3218/announce&tr=http://tracker.grepler.com:6969/announce&tr=http://tracker.kuroy.me:5944/announce&tr=http://210.244.71.26:6969/announce";
+
+            string magnetLink = @"magnet:?xt=urn:btih:B4D3A91D9CE527AA7C5B6EDACB969E5486B76EF2";
+
+            // TestCode - Mono Torrent
+            EngineSettings settings = new EngineSettings();
+            settings.AllowedEncryption = EncryptionTypes.All;
+            settings.SavePath = System.IO.Path.Combine(Environment.CurrentDirectory, "download");
+
+            var engine = new ClientEngine(settings);
+            engine.ChangeListenEndpoint(new IPEndPoint(IPAddress.Any, 6969));
+
+            TorrentSettings torrentSettings = new TorrentSettings(1,10);
+            var torrentSavePath = System.IO.Path.Combine(settings.SavePath, "torrent");
+
+            if (!Directory.Exists(settings.SavePath))
+                Directory.CreateDirectory(settings.SavePath);
+
+            if (!Directory.Exists(torrentSavePath))
+                Directory.CreateDirectory(torrentSavePath);
+            
+            var magnet = new MagnetLink(magnetLink);
+            var hash = InfoHash.FromMagnetLink(magnetLink);
+            var hashHex = InfoHash.FromHex("B4D3A91D9CE527AA7C5B6EDACB969E5486B76EF2");
+
+            var manager = new TorrentManager(magnet, settings.SavePath, torrentSettings, torrentSavePath);
+
+            engine.Register(manager);
+
+            // DH
+            var DL = new DhtListener(new IPEndPoint(IPAddress.Any, 6969));
+            var DH = new DhtEngine(DL);
+            engine.RegisterDht(DH);
+
+            //manager.Start();
+
+            DH.Start();
+            DL.Start();
+
+            DL.MessageReceived += DL_MessageReceived;
+
+            manager.Start();
+
+            await Task.Run(()=>
+            {  
+                while (manager.State != TorrentState.Stopped
+                && manager.State != TorrentState.Paused)
+                {
+                    _testLog.Invoke(() =>
+                    {
+                        Console.WriteLine(manager.Progress);
+                    });
+                    
+                    Thread.Sleep(1000);
+
+                    if(manager.Progress >= 100)
+                    {
+                        manager.Stop();
+                        break;
+                    }
+                }
+            });
+        }
+
+        private void DL_MessageReceived(byte[] buffer, IPEndPoint endpoint)
+        {
+            using (FileStream file = new FileStream("test.torrent", FileMode.Create))
+            {
+                file.Write(buffer, 0, buffer.Length);
+            }
         }
 
         private void Subscription_Click(object sender, RoutedEventArgs e)
